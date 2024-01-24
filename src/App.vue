ecb6002b958d01367ffb0227158d62675809ceb2
MarPinPar <marpinpar@MarPinPars-MacBook-Pro.local>
2024-01-24 14:34:49 -0500

    Minor fix

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,12 @@
+
+<!-- This project was made by Marcos Pinzón as an assignment for OneWater.org.
+
+     It was completed in around 14 hours. No external help was used apart from
+     Quasar documentation or google searches for specific language features.
+    
+     Following the recomentation on Quasar's introductory guide of using Vue Single File 
+     Components (SFC) I included HTML, CSS and JS in this file. -->
+
 <template>
   <router-view />
 </template>
